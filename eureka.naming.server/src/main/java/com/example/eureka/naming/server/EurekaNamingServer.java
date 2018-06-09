193985aa7516ac2068f8e1385ee36a5306cbc38b
shirishphatangare <sphatangare@gmail.com>
2018-06-09 17:09:46 -0500

    comment

--- eureka.naming.server/src/main/java/com/example/eureka/naming/server/EurekaNamingServer.java
+++ eureka.naming.server/src/main/java/com/example/eureka/naming/server/EurekaNamingServer.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaNamingServer {
 
 	public static void main(String[] args) {
-		System.out.println("Inside EurekaNamingServer");
+		System.out.println("Inside EurekaNamingServer 1");
 		SpringApplication.run(EurekaNamingServer.class, args);
 	}
 }
